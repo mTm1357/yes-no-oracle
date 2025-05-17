7a005bc449c5ba5d141514bf256beeecf344fd76
mTm1357 <t380454209@163.com>
2025-05-17 23:19:31 +0800

    init modules

--- app.vue
+++ app.vue
@@ -1,7 +1,6 @@
 <template>
   <div>
-    <h1 class="text-2xl font-bold underline">
-      Hello world!
-    </h1>
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,13 @@
-import tailwindcss from "@tailwindcss/vite";
-
+// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-  css: ['~/assets/css/main.css'],
-  vite: {
-    plugins: [
-      tailwindcss(),
-    ],
-  },
-
+  modules: [
+    '@nuxt/content',
+    '@nuxt/fonts',
+    '@nuxt/icon',
+    '@nuxt/image',
+    '@nuxt/scripts',
+    '@nuxt/ui'
+  ]
 })
